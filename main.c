7901e97db0d4d52b4e8b577ba9cb67ee034fa31e
HYL <sc20hl@leeds.ac.uk>
2022-05-13 14:24:31 +0800

    Update main.c

--- main.c
+++ main.c
@@ -3,8 +3,13 @@
 #include <SDL2/SDL_image.h>
 #include <SDL2/SDL_mixer.h>
 
+int MAP_SIZE = 20;
+char *map_filename = "map.txt";
+#define CELL_SIZE 20
+
+int **grid;
 
 int main() {
 
-	return 0;
+    return 0;
 }
